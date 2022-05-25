63a8366754a1f465a2e831b25732758f65ea3f7b
David <david.chalyan25@gmail.com>
2022-05-25 15:35:57 +0300

    Добавьте файлы проекта.

---
+++ MSSQLForCSProgs/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace MSSQLForCSProgs
+{
+    public partial class Form1 : Form
+    {
+        private SqlConnection sqlConnection = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["TestDB"].ConnectionString);
+            sqlConnection.Open();
+            if (sqlConnection.State == ConnectionState.Open)
+            {
+                MessageBox.Show("Подключение установлено!");
+            }
+        }
+    }
+}
