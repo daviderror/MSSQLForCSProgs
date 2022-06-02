08249b0fcf916567ceb37946ea0a5340551c41b7
David <david.chalyan25@gmail.com>
2022-06-02 16:48:05 +0300

    ListView filtering. List Search with LINQ

--- MSSQLForCSProgs/Form1.Designer.cs
+++ MSSQLForCSProgs/Form1.Designer.cs
@@ -46,10 +46,23 @@ namespace MSSQLForCSProgs
             this.tabPage3 = new System.Windows.Forms.TabPage();
             this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
             this.listView1 = new System.Windows.Forms.ListView();
-            this.button3 = new System.Windows.Forms.Button();
             this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button3 = new System.Windows.Forms.Button();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.tabPage5 = new System.Windows.Forms.TabPage();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ProductName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.QuantityPerUnit = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.UnitPrice = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.Form.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.tabPage2.SuspendLayout();
@@ -57,6 +70,9 @@ namespace MSSQLForCSProgs
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.tabPage3.SuspendLayout();
             this.tableLayoutPanel2.SuspendLayout();
+            this.tabPage4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.tabPage5.SuspendLayout();
             this.SuspendLayout();
             // 
             // Form
@@ -64,6 +80,8 @@ namespace MSSQLForCSProgs
             this.Form.Controls.Add(this.tabPage1);
             this.Form.Controls.Add(this.tabPage2);
             this.Form.Controls.Add(this.tabPage3);
+            this.Form.Controls.Add(this.tabPage4);
+            this.Form.Controls.Add(this.tabPage5);
             this.Form.Dock = System.Windows.Forms.DockStyle.Fill;
             this.Form.Location = new System.Drawing.Point(0, 0);
             this.Form.Name = "Form";
@@ -237,16 +255,6 @@ namespace MSSQLForCSProgs
             this.listView1.UseCompatibleStateImageBehavior = false;
             this.listView1.View = System.Windows.Forms.View.Details;
             // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(3, 384);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(130, 37);
-            this.button3.TabIndex = 1;
-            this.button3.Text = "SELECT";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
             // columnHeader1
             // 
             this.columnHeader1.Text = "ProductName";
@@ -258,6 +266,144 @@ namespace MSSQLForCSProgs
             // columnHeader3
             // 
             this.columnHeader3.Text = "UnitPrice";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(3, 384);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(130, 37);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "SELECT";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Controls.Add(this.comboBox1);
+            this.tabPage4.Controls.Add(this.textBox8);
+            this.tabPage4.Controls.Add(this.label1);
+            this.tabPage4.Controls.Add(this.dataGridView2);
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4.Size = new System.Drawing.Size(792, 424);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "Filter";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "0 - 10",
+            "10 - 50",
+            "50 - беск.",
+            "Всё"});
+            this.comboBox1.Location = new System.Drawing.Point(28, 336);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(126, 21);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(137, 299);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(215, 20);
+            this.textBox8.TabIndex = 2;
+            this.textBox8.TextChanged += new System.EventHandler(this.textBox8_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 299);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Название продукта";
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.dataGridView2.Location = new System.Drawing.Point(3, 3);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(786, 282);
+            this.dataGridView2.TabIndex = 0;
+            // 
+            // tabPage5
+            // 
+            this.tabPage5.Controls.Add(this.label2);
+            this.tabPage5.Controls.Add(this.comboBox2);
+            this.tabPage5.Controls.Add(this.textBox9);
+            this.tabPage5.Controls.Add(this.listView2);
+            this.tabPage5.Location = new System.Drawing.Point(4, 22);
+            this.tabPage5.Name = "tabPage5";
+            this.tabPage5.Size = new System.Drawing.Size(792, 424);
+            this.tabPage5.TabIndex = 4;
+            this.tabPage5.Text = "ListViewFilter";
+            this.tabPage5.UseVisualStyleBackColor = true;
+            // 
+            // listView2
+            // 
+            this.listView2.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.ProductName,
+            this.QuantityPerUnit,
+            this.UnitPrice});
+            this.listView2.GridLines = true;
+            this.listView2.HideSelection = false;
+            this.listView2.Location = new System.Drawing.Point(-4, 0);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(796, 225);
+            this.listView2.TabIndex = 0;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.View = System.Windows.Forms.View.Details;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(76, 228);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(164, 20);
+            this.textBox9.TabIndex = 1;
+            this.textBox9.TextChanged += new System.EventHandler(this.textBox9_TextChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "0 - 10",
+            "11 - 100",
+            "101 и выше",
+            "Все результаты"});
+            this.comboBox2.Location = new System.Drawing.Point(8, 276);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(306, 21);
+            this.comboBox2.TabIndex = 2;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 231);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Поиск";
+            // 
+            // ProductName
+            // 
+            this.ProductName.Text = "ProductName";
+            this.ProductName.Width = 86;
+            // 
+            // QuantityPerUnit
+            // 
+            this.QuantityPerUnit.Text = "QuantityPerUnit";
+            this.QuantityPerUnit.Width = 97;
+            // 
+            // UnitPrice
+            // 
+            this.UnitPrice.Text = "UnitPrice";
+            this.UnitPrice.Width = 104;
             // 
             // Form1
             // 
@@ -277,6 +423,11 @@ namespace MSSQLForCSProgs
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.tabPage3.ResumeLayout(false);
             this.tableLayoutPanel2.ResumeLayout(false);
+            this.tabPage4.ResumeLayout(false);
+            this.tabPage4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.tabPage5.ResumeLayout(false);
+            this.tabPage5.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -304,6 +455,19 @@ namespace MSSQLForCSProgs
         private System.Windows.Forms.ColumnHeader columnHeader1;
         private System.Windows.Forms.ColumnHeader columnHeader2;
         private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TabPage tabPage5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.ColumnHeader ProductName;
+        private System.Windows.Forms.ColumnHeader QuantityPerUnit;
+        private System.Windows.Forms.ColumnHeader UnitPrice;
     }
 }
 
--- MSSQLForCSProgs/Form1.cs
+++ MSSQLForCSProgs/Form1.cs
@@ -1,15 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
 using System.Windows.Forms;
-
+using System.Linq;
 namespace MSSQLForCSProgs
 {
     public partial class Form1 : Form
     {
         private SqlConnection sqlConnection = null;
         private SqlConnection nrthwindConnection = null;
+        private List<string[]> rows = new List<string[]>();
+        private List<string[]> filteredList = null;
         public Form1()
         {
             InitializeComponent();
@@ -20,6 +23,51 @@ namespace MSSQLForCSProgs
             sqlConnection.Open();
             nrthwindConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["NorthwindDB"].ConnectionString);
             nrthwindConnection.Open();
+            SqlDataAdapter dataAdapter1 = new SqlDataAdapter("SELECT * FROM Products", nrthwindConnection);
+            DataSet workDtGrV = new DataSet();
+            dataAdapter1.Fill(workDtGrV);
+            dataGridView2.DataSource = workDtGrV.Tables[0];
+
+            listView1.Items.Clear();
+            SqlDataReader sqlDataReader = null;
+            rows = new List<string[]>();
+            string[] row = null;
+            try
+            {
+                SqlCommand sqlCommand = new SqlCommand("SELECT ProductName , QuantityPerUnit , UnitPrice FROM Products", nrthwindConnection);
+                sqlDataReader = sqlCommand.ExecuteReader();
+                ListViewItem item = null;
+                while (sqlDataReader.Read())
+                {
+                    row = new string[]
+                    {
+                        Convert.ToString(sqlDataReader["ProductName"]),
+                        Convert.ToString(sqlDataReader["QuantityPerUnit"]),
+                        Convert.ToString(sqlDataReader["UnitPrice"])
+                    };
+                    rows.Add(row);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (sqlDataReader != null && !sqlDataReader.IsClosed)
+                {
+                    sqlDataReader.Close();
+                }
+            }
+            RefreshList(rows);
+        }
+        private void RefreshList(List <string[]> list)
+        {
+            listView2.Items.Clear();
+            foreach (string[] s in list)
+            {
+                listView2.Items.Add(new ListViewItem(s));
+            }
         }
         private void button1_Click(object sender, EventArgs e)
         {
@@ -46,31 +94,61 @@ namespace MSSQLForCSProgs
         }
         private void button3_Click(object sender, EventArgs e)
         {
-            listView1.Items.Clear();
-            SqlDataReader sqlDataReader = null;
-            try
+            
+        }
+        private void textBox8_TextChanged(object sender, EventArgs e)
+        {
+            (dataGridView2.DataSource as DataTable).DefaultView.RowFilter = $"ProductName LIKE '%{textBox8.Text}%'";
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (comboBox1.SelectedIndex)
             {
-                SqlCommand sqlCommand = new SqlCommand("SELECT ProductName , QuantityPerUnit , UnitPrice FROM Products", nrthwindConnection);
-                sqlDataReader = sqlCommand.ExecuteReader();
-                ListViewItem item = null;
-                while (sqlDataReader.Read())
-                {
-                    item = new ListViewItem(new string[] {Convert.ToString(sqlDataReader["ProductName"]),
-                        Convert.ToString(sqlDataReader["QuantityPerUnit"]),
-                        Convert.ToString(sqlDataReader["UnitPrice"]) });
-                    listView1.Items.Add(item);
-                }
+                case 0:
+                    (dataGridView2.DataSource as DataTable).DefaultView.RowFilter = $"UnitsInStock <= 10";
+                    break;
+                case 1:
+                    (dataGridView2.DataSource as DataTable).DefaultView.RowFilter = $"UnitsInStock >= 10 AND UnitsInStock <= 50";
+                    break;
+                case 2:
+                    (dataGridView2.DataSource as DataTable).DefaultView.RowFilter = $"UnitsInStock >= 50";
+                    break;
+                case 3:
+                    (dataGridView2.DataSource as DataTable).DefaultView.RowFilter = "";
+                    break;
             }
-            catch(Exception ex)
+        }
+
+        private void textBox9_TextChanged(object sender, EventArgs e)
+        {
+            filteredList = rows.Where((x) =>
+            x[0].ToLower().Contains(textBox9.Text.ToLower())).ToList();
+            RefreshList(filteredList);
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (comboBox2.SelectedIndex)
             {
-                MessageBox.Show(ex.Message);
-            }
-            finally
-            {
-                if (sqlDataReader!=null && !sqlDataReader.IsClosed)
-                {
-                    sqlDataReader.Close();
-                }
+                case 0:
+                    filteredList = rows.Where((x) =>
+                    Double.Parse(x[2]) <= 10).ToList();
+                    RefreshList(filteredList);
+                    break;
+                case 1:
+                    filteredList = rows.Where((x) =>
+                    Double.Parse(x[2]) > 10 && Double.Parse(x[2])<=100).ToList();
+                    RefreshList(filteredList);
+                    break;
+                case 2:
+                    filteredList = rows.Where((x) =>
+                    Double.Parse(x[2]) > 100).ToList();
+                    RefreshList(filteredList);
+                    break;
+                case 3:
+                    RefreshList(rows);
+                    break;
             }
         }
     }
